cc1ec30bbc263f720382410b32311c5f3317e40e
叶林森 <yelinsen@jiayouhui360.com>
2019-10-16 15:00:02 +0800

    关闭debug

--- application/config/config.php
+++ application/config/config.php
@@ -5,6 +5,7 @@ return [
         'yadmin'
     ],
     'csrf'       => false,
+    'debug'      => false,
     'db'         => require_once APP_PATH . '/config/database.php',
     'uploadUrl'  => 'http://img.woodlsy.com/upload/img?project=' . APP_NAME,
     'uploadPath' => 'http://img.woodlsy.com/'
