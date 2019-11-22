ee3d17557736bf964b8f20230432a8c4a550812f
叶林森 <yelinsen@jiayouhui360.com>
2019-11-22 14:46:28 +0800

    标题

--- application/modules/index/controllers/IndexController.php
+++ application/modules/index/controllers/IndexController.php
@@ -18,7 +18,7 @@ class IndexController extends BaseController
 
     public function listAction()
     {
-        $page = (int)$this->get('page', 'int', 1);
+        $page     = (int) $this->get('page', 'int', 1);
         $articles = (new ArticleLogic())->getArticle($page);
 
         $this->view->articles      = $articles;
@@ -28,11 +28,11 @@ class IndexController extends BaseController
 
     public function infoAction()
     {
-        $id      = (int)$this->get('id', 'int');
+        $id      = (int) $this->get('id', 'int');
         $article = (new ArticleLogic())->getArticleById($id);
 
-        $ip = $this->request->getClientAddress();
-        $key = md5($id.'_'.$ip);
+        $ip  = $this->request->getClientAddress();
+        $key = md5($id . '_' . $ip);
         if (!Redis::getInstance()->exists($key)) {
             (new ArticleLogic())->addArticleClick($id);
             $article['clicks']++;
@@ -41,6 +41,10 @@ class IndexController extends BaseController
 
         $tags = !empty($article['tags']) ? explode(',', $article['tags']) : '';
 
+        $this->view->title       = $article['title'];
+        $this->view->keywords    = $article['tags'];
+        $this->view->description = $article['desc'];
+
         $this->view->article       = $article;
         $this->view->tags          = $tags;
         $this->view->clickArticles = (new ArticleLogic())->getArticle(1, 5, 'clicks desc');
@@ -48,12 +52,12 @@ class IndexController extends BaseController
 
     public function searchAction()
     {
-        $page = (int)$this->get('page', 'int', 1);
+        $page    = (int) $this->get('page', 'int', 1);
         $keyword = $this->get('search', 'string', '');
 
         $articles = (new ArticleLogic())->getArticleBySearch($keyword, $page);
 
-        $articles = (new ArticleLogic())->getArticle($page);
+        $articles                  = (new ArticleLogic())->getArticle($page);
         $this->view->articles      = $articles;
         $this->view->articleCount  = (new ArticleLogic())->getArticleBySearchCount($keyword);
         $this->view->clickArticles = (new ArticleLogic())->getArticle(1, 5, 'clicks desc');
