93c89623c0e7f8c0a6bb426238465087603c1969
叶林森 <yelinsen@jiayouhui360.com>
2020-04-07 16:12:54 +0800

    suffix

--- application/config/config.php
+++ application/config/config.php
@@ -6,6 +6,7 @@ return [
     ],
     'csrf'       => false,
     'debug'      => false,
+    'suffix'     => 'html', // url后缀
     'db'         => require_once APP_PATH . '/config/database.php',
     'uploadUrl'  => 'http://img.woodlsy.com/upload/img?project=' . APP_NAME,
     'uploadPath' => 'http://img.woodlsy.com/'
